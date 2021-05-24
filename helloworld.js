0aa8d5663c93bcf94043f28690c76956185346a9
Linh Hoang <nikkalee817@gmail.com>
2021-05-24 14:43:04 -0500

    final code

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,36 @@
-console.log("hello world")
+const readline = require('readline');
+
+// creating variables
+
+const myFirstName = 'Linh';
+let myAge = 33;
+
+// console.log('my name is ' + myFirstName + ' and I am ' + myAge)
+// console.log(`My name is ${myFirstName} and I am ${myAge}`)
+// console.log(myFirstName, myAge)
+
+const isCool = true;
+
+let myHeight = null; // null
+let myWeight;  // undefined 
+
+// NUMBERS 
+
+let num1 = 7;
+let num2 = 10;
+let num3 = 6 + 45;
+let num4 = num1 + num2;
+
+// GETTING USER INPUT
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+reader.question("Enter a number to find out if it is odd or even: ", function(number) {
+    if(number % 2 === 0) {
+        console.log('Even!')
+    } else {
+        console.log('Odd!')
+    }
+});
